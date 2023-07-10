05d8c463b69a6bda2976751b4846c84dbcf1a9a9
yaron <yaronkinar@gmail.com>
2023-07-10 20:47:04 +0300

    remove dev

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
 import {store} from './store'
 import {Provider} from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
             <Provider store={store}>
             <App/>
             </Provider>
-        </DevSupport>
+
     </React.StrictMode>,
 )
