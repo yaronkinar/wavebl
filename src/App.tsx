93832737c1a377c0aeb24ec2df4a958a5eb8b5c5
yaron <yaronkinar@gmail.com>
2023-07-10 17:00:43 +0300

    add wrapper and useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react';
 
-import './App.css'
-import {
-    Box
-
-
-} from '@mui/material';
+import './App.css';
 import FormDialog from "./components/FormDialog.tsx";
 import Column from "./components/Column.tsx";
-
-
-
+import Wrapper from "./components/Wrapper.tsx";
 
 function App() {
     const [open, setOpen] = useState<boolean>(false);
     const [value, setValue] = useState<string>('');
 
     type DialogType = "blank" | "edit"
-    const handleClickOpen = (type:DialogType) => {
-        if(type === "blank") setValue('')
-        if(type === "edit") setValue("Hello")
+    const handleClickOpen = useCallback((type: DialogType) => {
+        if (type === "blank") setValue('')
+        if (type === "edit") setValue("Hello")
         setOpen(true);
-    };
+    }, [setValue, setOpen]);
 
-    const isValidate  = value.trim().length == 0;
+    const isValidate = value.trim().length === 0;
 
-    const handleChange = (event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-
-
+    const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setValue(event.target.value);
-    };
+    }, [setValue]);
 
-    const handleSave = () => {
-        console.log(value)
+    const handleSave = useCallback(() => {
+        console.log(value);
         setOpen(false);
-    }
-    const handleClose = () => {
+    }, [value, setOpen]);
+
+    const handleClose = useCallback(() => {
         setOpen(false);
-    };
-  return (
-      <div>
-          <Box
-              sx={{
-                  display: 'grid',
-                  gridAutoFlow: 'row',
-                  gridTemplateColumns: 'repeat(5, 1fr)',
-                  gridTemplateRows: 'auto',
-                  gap: 1,
-                  padding: "20px",
-                  border: '1px solid black',
-                  height: '80vh',
-              }}
-          >
-              <Column onClick={() => handleClickOpen("blank")} gridColumn={"1"} buttonLabel="Blank"/>
+    }, [setOpen]);
 
-              <Column onClick={() => handleClickOpen("edit")} gridColumn={"5"} buttonLabel="Edit"/>
-          </Box>
+    return (
+        <div>
+            <Wrapper>
+                <Column onClick={() => handleClickOpen("blank")} gridColumn={"1"} buttonLabel="Blank"/>
+                <Column onClick={() => handleClickOpen("edit")} gridColumn={"5"} buttonLabel="Edit"/>
+            </Wrapper>
 
-
-          <FormDialog open={open} onClose={handleClose} value={value} onChange={handleChange} error={isValidate}
-                      onClick={handleSave}/>
-      </div>
-  )
+            <FormDialog open={open} onClose={handleClose} value={value} onChange={handleChange} error={isValidate} onClick={handleSave}/>
+        </div>
+    )
 }
 
-export default App
+export default App;
---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Box} from "@mui/material";
+
+export default function Wrapper({children}: { children: React.ReactNode }): React.ReactNode{
+    return  (<Box
+        sx={{
+            display: 'grid',
+            gridAutoFlow: 'row',
+            gridTemplateColumns: 'repeat(5, 1fr)',
+            gridTemplateRows: 'auto',
+            gap: 1,
+            padding: "20px",
+            border: '1px solid black',
+            height: '80vh',
+        }}
+    >
+        {children}
+
+    </Box>)
+}
